56b6869bd9a3c6974a59afb371e7d826039e5ce3
Victor <deep-research>
2017-11-30 17:52:01 -0600

    create database

---
+++ insert-ten.sql
@@ -0,0 +1,13 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Guitar Amplifier", "music", 600, 20), # 1
+("Electric Guitar", "music", 400, 40), # 2
+("Witches' Brew", "occult", 25, 30), # 3
+("Medieval Grimoire", "books", 50, 15), # 4
+("Windows Computer", "tech", 200, 300), # 5
+("Linux Computer", "tech", 150, 200), # 6
+("Tarot Card Deck", "occult", 40, 75), # 7
+("Record Player", "music", 60, 20), # 8
+("Harry Potter Book", "books", 10, 500), # 9
+("Alchemical Gold", "occult", 1000, 5) # 10

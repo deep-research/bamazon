9f38e632cf3a7d0742273a92adea415e155fc1af
Victor <deep-research>
2017-11-30 20:08:18 -0600

    Inquirer Purchase Input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql")
+var inquirer = require("inquirer")
 
 var connect = mysql.createConnection({
     host: "localhost",
@@ -11,17 +12,40 @@ var connect = mysql.createConnection({
 connect.connect(function (err) {
     if(err) throw err;
 
-    selectAll()
-    connect.end();
+    viewCatalogue()
 })
 
-function selectAll() {
+var viewCatalogue = function() {
     connect.query("SELECT * FROM products", function(err, res) {
         if(err) throw err
+        console.log("PRODUCT LISTING")
         for (i=0; i<res.length; i++) {
             console.log("Item "+ res[i].item_id + ": " +
                         res[i].product_name +
                         " ($" + res[i].price + ")")
         }
+        console.log("")
+        console.log("BUY AN ITEM")
+        makePurchase()
     })
+}
+
+var makePurchase = function() {
+    var questions = [
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter an item number:"
+        },
+        {
+            type: 'input',
+            name: 'count',
+            message: "How many would you like?"
+        }
+    ]
+    inquirer.prompt(questions).then(answers => {
+        console.log("The item number is: " + answers.id)
+        console.log("Order amount: " + answers.count)
+        connect.end()
+    });    
 }
